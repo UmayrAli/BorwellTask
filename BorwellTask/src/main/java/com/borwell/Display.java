66a2d75cdfa74fdde48bf8e288be40a0dc877855
Nightmayr <umayr112@icloud.com>
2017-11-04 21:24:00 +0000

    Init

---
+++ BorwellTask/src/main/java/com/borwell/Display.java
@@ -0,0 +1,4 @@
+package com.borwell;
+
+public class Display {
+}
---
+++ BorwellTask/src/main/java/com/borwell/Inputs.java
@@ -0,0 +1,21 @@
+package com.borwell;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public Scanner Input = new Scanner(System.in);
+
+    private String[] lineEndings = {"floor width: ", "floor depth: ", "wall width: ", "wall height: ", "window width: ", "window height: ", "room width: ", "room height: ", "room depth: ", "number of windows in the room: ", "number of doors in the room: ", "number of walls in the room: "};
+    private String[] Inputs = new String[12];
+    private Double[] Parsed = new Double[12];
+
+    //
+    public void Collect()
+    {
+        for(int q=0; q<=11; q++)
+        {
+            boolean Parsed = fales;
+            System.out.println("Please ");
+        }
+    }
+}
