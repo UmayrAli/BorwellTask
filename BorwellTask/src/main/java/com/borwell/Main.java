49e21b56bbd44fd1b7aafd2f17f0e73ff88da403
Nightmayr <umayr112@icloud.com>
2017-11-06 19:01:34 +0000

    Main method to also display

--- BorwellTask/src/main/java/com/borwell/Main.java
+++ BorwellTask/src/main/java/com/borwell/Main.java
@@ -1,10 +1,11 @@
 package com.borwell;
 
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main (String[] args)
     {
         Inputs ClassInput = new Inputs();
-        Display ClassDisplay = new Display();
         PaintCalculation ClassCalculation = new PaintCalculation();
 
         //Receives the user's input
@@ -14,9 +15,16 @@ public class Main {
         double FloorArea = ClassCalculation.AreaOfFloor(ClassInput.Parsed[0], ClassInput.Parsed[1]);
         double RequiredPaint = ClassCalculation.RequiredPaint(ClassInput.Parsed[2], ClassInput.Parsed[3], ClassInput.Parsed[4], ClassInput.Parsed[5], ClassInput.Parsed[11], ClassInput.Parsed[10], ClassInput.Parsed[9]);        double RoomVolume = ClassCalculation.VolumeOfRoom(ClassInput.Parsed[6], ClassInput.Parsed[7], ClassInput.Parsed[8]);
 
+        //Formatting to 2 places after the decimal place
+        DecimalFormat decimalFormat = new DecimalFormat();
+        decimalFormat.setMaximumFractionDigits(2);
 
-        //Prints the values
-        ClassDisplay.Send(FloorArea, RequiredPaint, RoomVolume);
+        //Prints out the values
+        System.out.println("The area of the floor is: \n" + decimalFormat.format(FloorArea) + " metres squared \n");
+        System.out.println("The required amount of paint is: \n" + decimalFormat.format(RequiredPaint) + " litres \n");
+        System.out.println("The recommended amount of paint is: \n" + decimalFormat.format(RequiredPaint+ 1) + " litres \n");
+        System.out.println("The volume of the room is: \n" + decimalFormat.format(RoomVolume) + " metres cubed \n");
+
     }
 }
 
